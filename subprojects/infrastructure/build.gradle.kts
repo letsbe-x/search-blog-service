603dc34e9a487645c5a8bf7fed7288e2805865ff
letsbe-x <10years.shimhyeonggwan@gmail.com>
2023-03-19 11:45:20 +0900

    fix(request): intellij 파일위치와 환경변수를 정상적으로 수정합니다.

--- subprojects/infrastructure/build.gradle.kts
+++ subprojects/infrastructure/build.gradle.kts
@@ -3,4 +3,6 @@ tasks.bootJar.get().enabled = false
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    testImplementation("com.squareup.okhttp3:mockwebserver")
 }
