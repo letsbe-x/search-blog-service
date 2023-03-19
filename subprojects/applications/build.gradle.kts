70ab30fae5461156c0db8179d0cfeca2d633af1c
letsbe-x <10years.shimhyeonggwan@gmail.com>
2023-03-19 11:37:02 +0900

    build: 각 멀티모듈 프로젝트의 관계에 대해서 수정

--- subprojects/applications/build.gradle.kts
+++ subprojects/applications/build.gradle.kts
@@ -2,5 +2,6 @@ tasks.bootJar.get().enabled = false
 
 dependencies {
     implementation(project(":infrastructure"))
-    implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
--- subprojects/infrastructure/build.gradle.kts
+++ subprojects/infrastructure/build.gradle.kts
@@ -1,8 +1,6 @@
 tasks.bootJar.get().enabled = false
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux") // TODO: Netty로 동작안하게 되므로 의존생 다시 생각
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
